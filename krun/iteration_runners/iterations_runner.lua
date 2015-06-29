545e5c72b1beb7ed881b31bccef79f470bb3a28c
Edd Barrett <vext01@gmail.com>
2015-06-29 15:57:54 +0100

    Lua iter runner: Rename vars to minimise scope clash.
    Similar to what I did for JS. Lua does have block-level scoping, but
    no module/file namespace. We still are using global variables...

--- krun/iteration_runners/iterations_runner.lua
+++ krun/iteration_runners/iterations_runner.lua
@@ -8,28 +8,28 @@ local ffi = require("ffi")
 ffi.cdef[[double clock_gettime_monotonic();]]
 local kruntime = ffi.load("kruntime")
 
-benchmark = arg[1]
-iters = tonumber(arg[2])
-param = tonumber(arg[3])
+BM_benchmark = arg[1]
+BM_iters = tonumber(arg[2])
+BM_param = tonumber(arg[3])
 
 if #arg ~= 3 then
     print("usage: iterations_runner.lua <benchmark> <# of iterations> <benchmark param>")
     os.exit(1)
 end
 
-dofile(benchmark)
+dofile(BM_benchmark)
 
 io.stdout:write("[")
 io.stdout:flush()
-for i = 1, iters, 1 do -- inclusive upper bound in lua
-    io.stderr:write(string.format("    Iteration %d/%d\n", i, iters))
+for BM_i = 1, BM_iters, 1 do -- inclusive upper bound in lua
+    io.stderr:write(string.format("    Iteration %d/%d\n", BM_i, BM_iters))
 
-    start_time = kruntime.clock_gettime_monotonic()
-    run_iter(param) -- run one iteration of benchmark
-    end_time = kruntime.clock_gettime_monotonic()
+    BM_start_time = kruntime.clock_gettime_monotonic()
+    run_iter(BM_param) -- run one iteration of benchmark
+    BM_end_time = kruntime.clock_gettime_monotonic()
 
-    intvl = end_time - start_time
-    io.stdout:write(intvl .. ", ")
+    BM_intvl = BM_end_time - BM_start_time
+    io.stdout:write(BM_intvl .. ", ")
     io.stdout:flush()
 end
 
