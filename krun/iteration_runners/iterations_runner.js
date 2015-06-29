f112315afecc9cd4f7d53f023894dd28475a5304
Edd Barrett <vext01@gmail.com>
2015-06-29 15:53:11 +0100

    JS iterations runner: rename vars to reduce probability of name clashes.
    Javascript has function-level scoping and no module/file namespace. So
    all of our variables in the runner are effectively global. Let's rename
    our somewhat generic variable names to more obscure ones to avoid name
    clashes.

--- krun/iteration_runners/iterations_runner.js
+++ krun/iteration_runners/iterations_runner.js
@@ -4,21 +4,21 @@ if (this.arguments.length != 3) {
     throw "usage: iterations_runner.js <benchmark> <# of iterations> <benchmark param>";
 }
 
-entry_point = this.arguments[0];
-n_iters = parseInt(this.arguments[1]);
-param = parseInt(this.arguments[2]);
+var BM_entry_point = this.arguments[0];
+var BM_n_iters = parseInt(this.arguments[1]);
+var BM_param = parseInt(this.arguments[2]);
 
-load(entry_point);
+load(BM_entry_point);
 
 print("[");
-for (i = 0; i < n_iters; i++) {
-	//print("    Execution " + (i + 1) + "/" + n_iters); // XXX needs to got to stderr
-	var start_time = clock_gettime_monotonic();
-	run_iter(param);
-	var stop_time = clock_gettime_monotonic();
+for (BM_i = 0; BM_i < BM_n_iters; BM_i++) {
+	//print("    Execution " + (BM_i + 1) + "/" + BM_n_iters); // XXX needs to got to stderr
+	var BM_start_time = clock_gettime_monotonic();
+	run_iter(BM_param);
+	var BM_stop_time = clock_gettime_monotonic();
 
-	var intvl = stop_time - start_time;
-	print(intvl);
+	var BM_intvl = BM_stop_time - BM_start_time;
+	print(BM_intvl);
 	print(", ")
 }
 print("]");
