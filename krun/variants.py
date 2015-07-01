cb8cb3cfb1f1a69139f4cce3c6b2b3ebcc126ae1
Edd Barrett <vext01@gmail.com>
2015-07-01 15:32:15 +0100

    Fill in heap limit implementations.

--- krun/variants.py
+++ krun/variants.py
@@ -7,6 +7,10 @@ DIR = os.path.abspath(os.path.dirname(__file__))
 ITERATIONS_RUNNER_DIR = os.path.abspath(os.path.join(DIR, "..", "iterations_runners"))
 BENCHMARKS_DIR = "benchmarks"
 
+# !!!
+# Don't mutate any lists passed down from the user's config file!
+# !!!
+
 class BaseVariant(object):
 
     def __init__(self, iterations_runner, entry_point=None, subdir=None, extra_env=None):
@@ -21,7 +25,7 @@ class BaseVariant(object):
         self.extra_env = extra_env
 
     def run_exec(self, interpreter, benchmark, iterations, param, vm_env, vm_args, heap_limit_kb):
-        raise NotImplemented("abstract")
+        raise NotImplementedError("abstract")
 
     def _run_exec(self, args, env=None):
         """ Deals with actually shelling out """
@@ -70,6 +74,7 @@ class JavaVariant(BaseVariant):
                              extra_env=extra_env)
 
     def run_exec(self, interpreter, benchmark, iterations, param, vm_env, vm_args, heap_limit_kb):
+        vm_args = vm_args[:] + ["-Xmx%sK" % heap_limit_kb]
         args = [interpreter] + vm_args + [self.iterations_runner, self.entry_point, str(iterations), str(param)]
         bench_dir = os.path.abspath(os.path.join(os.getcwd(), BENCHMARKS_DIR, benchmark, self.subdir))
 
@@ -95,8 +100,9 @@ class PythonVariant(GenericScriptingVariant):
                                          extra_env=extra_env)
 
     def run_exec(self, interpreter, benchmark, iterations, param, vm_env, vm_args, heap_limit_kb):
-        # XXX heap
-        return self._generic_scripting_run_exec(interpreter, benchmark, param, vm_env, vm_args)
+        # heap_limit_kb unused.
+        # Python reads the rlimit structure to decide its heap limit.
+        return self._generic_scripting_run_exec(interpreter, benchmark, iterations, param, vm_env, vm_args)
 
 class LuaVariant(GenericScriptingVariant):
     def __init__(self, entry_point=None, subdir=None, extra_env=None):
@@ -107,7 +113,11 @@ class LuaVariant(GenericScriptingVariant):
                                          extra_env=extra_env)
 
     def run_exec(self, interpreter, benchmark, iterations, param, vm_env, vm_args, heap_limit_kb):
-        # XXX heap
+        # I was unable to find any special switches to limit lua's heap size.
+        # Looking at implementationsi:
+        #  * luajit uses anonymous mmap() to allocate memory, fiddling
+        #    with rlimits prior.
+        #  * Stock lua doesn't seem to do anything special. Just realloc().
         return self._generic_scripting_run_exec(interpreter, benchmark, iterations, param, vm_env, vm_args)
 
 class PHPVariant(GenericScriptingVariant):
@@ -119,7 +129,7 @@ class PHPVariant(GenericScriptingVariant):
                                          extra_env=extra_env)
 
     def run_exec(self, interpreter, benchmark, iterations, param, vm_env, vm_args, heap_limit_kb):
-        # XXX heap
+        vm_args = vm_args[:] + ["-d", "memory_limit=%sK" % heap_limit_kb]
         return self._generic_scripting_run_exec(interpreter, benchmark, iterations, param, vm_env, vm_args)
 
 class RubyVariant(GenericScriptingVariant):
@@ -130,9 +140,10 @@ class RubyVariant(GenericScriptingVariant):
                                          subdir=subdir,
                                          extra_env=extra_env)
 
+class JRubyVariant(RubyVariant):
     def run_exec(self, interpreter, benchmark, iterations, param, vm_env, vm_args, heap_limit_kb):
-        # XXX heap
-        return self._generic_scripting_run_exec(interpreter, benchmark, param, vm_env, vm_args)
+        vm_args = vm_args[:] + ["-J-Xmx%sK" % heap_limit_kb]
+        return self._generic_scripting_run_exec(interpreter, benchmark, iterations, param, vm_env, vm_args)
 
 class JavascriptVariant(GenericScriptingVariant):
     def __init__(self, entry_point=None, subdir=None, extra_env=None):
@@ -143,9 +154,13 @@ class JavascriptVariant(GenericScriptingVariant):
                                          extra_env=extra_env)
 
 
-    # pretty much the same as the generic implementation, but needs a '--' argument.
+class V8Variant(JavascriptVariant):
     def run_exec(self, interpreter, benchmark, iterations, param, vm_env, vm_args, heap_limit_kb):
-        # XXX heap
+
+        # this is a best effort at limiting the heap space.
+        # V8 has a "new" and "old" heap. I can't see a way to limit the total of the two.
+        vm_args = vm_args[:] + ["--max_old_space_size", "%s" % int(heap_limit_kb / 1024)] # as MB
+
         script_path = os.path.join(BENCHMARKS_DIR, benchmark, self.subdir, self.entry_point)
         args = [interpreter] + vm_args + \
             [self.iterations_runner, '--', script_path, str(iterations), str(param)]
