47f9f69dd6070892b6388d7c3bbf3f6975f8820e
Edd Barrett <vext01@gmail.com>
2015-10-16 14:32:25 +0100

    Use local variables in lua runner.
    Minimises possibility of name clash.

--- iterations_runners/iterations_runner.lua
+++ iterations_runners/iterations_runner.lua
@@ -8,9 +8,9 @@ local ffi = require("ffi")
 ffi.cdef[[double clock_gettime_monotonic();]]
 local kruntime = ffi.load("kruntime")
 
-BM_benchmark = arg[1]
-BM_iters = tonumber(arg[2])
-BM_param = tonumber(arg[3])
+local BM_benchmark = arg[1]
+local BM_iters = tonumber(arg[2])
+local BM_param = tonumber(arg[3])
 
 if #arg ~= 3 then
     print("usage: iterations_runner.lua <benchmark> <# of iterations> <benchmark param>")
@@ -24,11 +24,11 @@ io.stdout:flush()
 for BM_i = 1, BM_iters, 1 do -- inclusive upper bound in lua
     io.stderr:write(string.format("[iterations_runner.lua] iteration %d/%d\n", BM_i, BM_iters))
 
-    BM_start_time = kruntime.clock_gettime_monotonic()
+    local BM_start_time = kruntime.clock_gettime_monotonic()
     run_iter(BM_param) -- run one iteration of benchmark
-    BM_end_time = kruntime.clock_gettime_monotonic()
+    local BM_end_time = kruntime.clock_gettime_monotonic()
 
-    BM_intvl = BM_end_time - BM_start_time
+    local BM_intvl = BM_end_time - BM_start_time
 
     io.stdout:write(BM_intvl)
     if BM_i < BM_iters then
