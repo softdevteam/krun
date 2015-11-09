ccf8f30afcc3b4a4b8c5cbd93c51630f789b8f04
Edd Barrett <vext01@gmail.com>
2015-11-09 15:35:57 +0000

    Restore non-jruby support for monotonic time.

--- iterations_runners/iterations_runner.rb
+++ iterations_runners/iterations_runner.rb
@@ -1,13 +1,24 @@
 class AssertionError < RuntimeError
 end
 
-def clock_gettime_monotonic()
-    # XXX this is for a patched JRuby/truffle only for now! 
-    #
-    # JRuby does not yet provide access to the (raw) monotonic clock, and
-    # adding support is non-trivial (not as simple as adding the C constant).
-    # For now we patch JRuby/Truffle to expose our libkruntime function.
-    Truffle::Primitive.clock_gettime_monotonic()
+# defined this way so we don't measure the conditional platform check.
+if RUBY_PLATFORM == "java" then
+    # "java" means JRuby.
+    def clock_gettime_monotonic()
+        # JRuby does not (yet) provide access to the (raw) monotonic clock, and
+        # adding support is non-trivial (not as simple as adding the C
+        # constant).  For now we patch JRuby/Truffle to expose our libkruntime
+        # function.
+        Truffle::Primitive.clock_gettime_monotonic()
+    end
+elsif /linux/ =~ RUBY_PLATFORM then
+    def clock_gettime_monotonic()
+        Process.clock_gettime(Process::CLOCK_MONOTONIC_RAW)
+    end
+else
+    def clock_gettime_monotonic()
+        Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    end
 end
 
 def assert(cond)
