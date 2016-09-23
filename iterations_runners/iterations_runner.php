635c2810178babf6c4696d522778dfca726c30a2
Edd Barrett <vext01@gmail.com>
2016-09-23 16:29:56 +0100

    iterations_runners: Update for per-core measurements.

--- iterations_runners/iterations_runner.php
+++ iterations_runners/iterations_runner.php
@@ -1,16 +1,15 @@
 <?php
 
 /*
- * Note! You will need to provide an implementation of clock_gettime_monotonic()
+ * Note, you will need to patch the VM so as to allow access to:
+ *  krun_init();
+ *  krun_done();
+ *  krun_measure();
+ *  krun_get_num_cores();
+ *  krun_get_wallclock();
+ *  krun_get_{core_cycles,aperf,mperf}_double();
  */
 
-/*
- * Executes a benchmark many times within a single process.
- *
- * In Kalibera terms, this script represents one executions level run.
- */
-
-# main
 if ($argc != 6) {
     fwrite(STDERR, "usage: iterations_runner.php <benchmark> <# of iterations> " .
            "<benchmark param> <debug flag> <instrument flag>\n");
@@ -19,7 +18,7 @@ if ($argc != 6) {
 
 $BM_benchmark = $argv[1];
 $BM_iters = $argv[2];
-$BM_param = (int) $argv[3]; // parameter sent to benchmark.
+$BM_param = (int) $argv[3];
 $BM_debug = ((int) $argv[4]) > 0;
 
 if (!file_exists($BM_benchmark)) {
@@ -28,21 +27,24 @@ if (!file_exists($BM_benchmark)) {
 
 include($BM_benchmark);
 
-/*
- * The benchmark should provide a function called "run_iter" which represents
- * one iterations level run of the benchmark.
- */
+// Find benchmark entry point
 if (!function_exists("run_iter")) {
     throw new RuntimeException("Benchmark is missing a 'run_iter' function");
 }
 
+krun_init();
+$BM_num_cores = krun_get_num_cores();
+
 // Pre-allocate results arrays
 $BM_wallclock_times = array_fill(0, $BM_iters, 0);
-$BM_cycle_counts = array_fill(0, $BM_iters, 0);
-$BM_aperf_counts = array_fill(0, $BM_iters, 0);
-$BM_mperf_counts = array_fill(0, $BM_iters, 0);
-
-libkruntime_init();
+$BM_cycle_counts = array_fill(0, $BM_num_cores, array());
+$BM_aperf_counts = array_fill(0, $BM_num_cores, array());
+$BM_mperf_counts = array_fill(0, $BM_num_cores, array());
+for ($BM_core = 0; $BM_core < $BM_num_cores; $BM_core++) {
+    $BM_cycle_counts[$BM_core] = array_fill(0, $BM_iters, 0);
+    $BM_aperf_counts[$BM_core] = array_fill(0, $BM_iters, 0);
+    $BM_mperf_counts[$BM_core] = array_fill(0, $BM_iters, 0);
+}
 
 // Main loop
 for ($BM_i = 0; $BM_i < $BM_iters; $BM_i++) {
@@ -51,82 +53,36 @@ for ($BM_i = 0; $BM_i < $BM_iters; $BM_i++) {
     }
 
     // Start timed section
-    $BM_mperf_start = read_mperf_double();
-    $BM_aperf_start = read_aperf_double();
-    $BM_cycles_start = read_core_cycles_double();
-    $BM_wallclock_start = clock_gettime_monotonic();
-
+    krun_measure(0);
     run_iter($BM_param);
-
-    $BM_wallclock_stop = clock_gettime_monotonic();
-    $BM_cycles_stop = read_core_cycles_double();
-    $BM_aperf_stop = read_aperf_double();
-    $BM_mperf_stop = read_mperf_double();
+    krun_measure(1);
     // End timed section
 
-    // Sanity checks
-    if ($BM_wallclock_start > $BM_wallclock_stop) {
-        fwrite(STDERR, "wallclock start greater than stop\n");
-        fwrite(STDERR, "start=${BM_wallclock_start} stop=${BM_wallclock_stop}\n");
-        exit(1);
-    }
-
-    if ($BM_cycles_start > $BM_cycles_stop) {
-        fwrite(STDERR, "cycles start greater than stop\n");
-        fwrite(STDERR, "start=${BM_cycles_start} stop=${BM_cycles_stop}\n");
-        exit(1);
-    }
-
-    if ($BM_aperf_start > $BM_aperf_stop) {
-        fwrite(STDERR, "aperf start greater than stop\n");
-        fwrite(STDERR, "start=${BM_aperf_start} stop=${BM_aperf_stop}\n");
-        exit(1);
-    }
-
-    if ($BM_mperf_start > $BM_mperf_stop) {
-        fwrite(STDERR, "mperf start greater than stop\n");
-        fwrite(STDERR, "start=${BM_mperf_start} stop=${BM_mperf_stop}\n");
-        exit(1);
-    }
-
     // Compute deltas
-    $BM_wallclock_times[$BM_i] = $BM_wallclock_stop - $BM_wallclock_start;
-    $BM_cycle_counts[$BM_i] = $BM_cycles_stop - $BM_cycles_start;
-    $BM_aperf_counts[$BM_i] = $BM_aperf_stop - $BM_aperf_start;
-    $BM_mperf_counts[$BM_i] = $BM_mperf_stop - $BM_mperf_start;
+    $BM_wallclock_times[$BM_i] = krun_get_wallclock(1) - krun_get_wallclock(0);
+
+    for ($BM_core = 0; $BM_core < $BM_num_cores; $BM_core++) {
+        $BM_cycle_counts[$BM_core][$BM_i] =
+            krun_get_core_cycles_double(1, $BM_core) -
+            krun_get_core_cycles_double(0, $BM_core);
+        $BM_aperf_counts[$BM_core][$BM_i] =
+            krun_get_aperf_double(1, $BM_core) -
+            krun_get_aperf_double(0, $BM_core);
+        $BM_mperf_counts[$BM_core][$BM_i] =
+            krun_get_mperf_double(1, $BM_core) -
+            krun_get_mperf_double(0, $BM_core);
+    }
 }
 
-libkruntime_done();
+krun_done();
 
-// Emit measurements
-echo "[[";
-for ($BM_i = 0; $BM_i < $BM_iters; $BM_i++) {
-    echo $BM_wallclock_times[$BM_i];
-    if ($BM_i < $BM_iters - 1) {
-        echo ", ";
-    }
-}
-echo "], [";
-for ($BM_i = 0; $BM_i < $BM_iters; $BM_i++) {
-    echo $BM_cycle_counts[$BM_i];
-    if ($BM_i < $BM_iters - 1) {
-        echo ", ";
-    }
-}
-echo "], [";
-for ($BM_i = 0; $BM_i < $BM_iters; $BM_i++) {
-    echo $BM_aperf_counts[$BM_i];
-    if ($BM_i < $BM_iters - 1) {
-        echo ", ";
-    }
-}
-echo "], [";
-for ($BM_i = 0; $BM_i < $BM_iters; $BM_i++) {
-    echo $BM_mperf_counts[$BM_i];
-    if ($BM_i < $BM_iters - 1) {
-        echo ", ";
-    }
-}
-echo "]]\n";
+$BM_output = array(
+    "wallclock_times" => $BM_wallclock_times,
+    "core_cycle_counts" => $BM_cycle_counts,
+    "aperf_counts" => $BM_aperf_counts,
+    "mperf_counts" => $BM_mperf_counts
+);
+
+echo json_encode($BM_output);
 
 ?>
