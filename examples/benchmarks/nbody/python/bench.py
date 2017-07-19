65f6c332e372a24bbe7bc9304013db009c1e250a
Edd Barrett <vext01@gmail.com>
2017-07-19 16:59:48 +0100

    Use the n-body/python benchmark from the warmup experiment and run it under PyPy.

--- examples/benchmarks/nbody/python/bench.py
+++ examples/benchmarks/nbody/python/bench.py
@@ -5,6 +5,13 @@
 # modified by Tupteq, Fredrik Johansson, and Daniel Nanz
 # modified by Maciej Fijalkowski
 
+import sys 
+
+EXPECT_CHECKSUM = -0.3381550232201908645635057837353087961673736572265625
+N_ADVANCES = 100000
+EPSILON = 0.0000000000001
+
+
 def combinations(l):
     result = []
     for x in xrange(len(l) - 1):
@@ -13,51 +20,55 @@ def combinations(l):
             result.append((l[x],y))
     return result
 
-PI = 3.14159265358979323
+PI = 3.141592653589793
 SOLAR_MASS = 4 * PI * PI
 DAYS_PER_YEAR = 365.24
 
-BODIES = {
-    'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+def setup_state():
 
-    'jupiter': ([4.84143144246472090e+00,
-                 -1.16032004402742839e+00,
-                 -1.03622044471123109e-01],
-                [1.66007664274403694e-03 * DAYS_PER_YEAR,
-                 7.69901118419740425e-03 * DAYS_PER_YEAR,
-                 -6.90460016972063023e-05 * DAYS_PER_YEAR],
-                9.54791938424326609e-04 * SOLAR_MASS),
+    bodies = {
+        'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
 
-    'saturn': ([8.34336671824457987e+00,
-                4.12479856412430479e+00,
-                -4.03523417114321381e-01],
-               [-2.76742510726862411e-03 * DAYS_PER_YEAR,
-                4.99852801234917238e-03 * DAYS_PER_YEAR,
-                2.30417297573763929e-05 * DAYS_PER_YEAR],
-               2.85885980666130812e-04 * SOLAR_MASS),
+        'jupiter': ([4.84143144246472090e+00,
+                     -1.16032004402742839e+00,
+                     -1.03622044471123109e-01],
+                    [1.66007664274403694e-03 * DAYS_PER_YEAR,
+                     7.69901118419740425e-03 * DAYS_PER_YEAR,
+                     -6.90460016972063023e-05 * DAYS_PER_YEAR],
+                    9.54791938424326609e-04 * SOLAR_MASS),
 
-    'uranus': ([1.28943695621391310e+01,
-                -1.51111514016986312e+01,
-                -2.23307578892655734e-01],
-               [2.96460137564761618e-03 * DAYS_PER_YEAR,
-                2.37847173959480950e-03 * DAYS_PER_YEAR,
-                -2.96589568540237556e-05 * DAYS_PER_YEAR],
-               4.36624404335156298e-05 * SOLAR_MASS),
+        'saturn': ([8.34336671824457987e+00,
+                    4.12479856412430479e+00,
+                    -4.03523417114321381e-01],
+                   [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                    4.99852801234917238e-03 * DAYS_PER_YEAR,
+                    2.30417297573763929e-05 * DAYS_PER_YEAR],
+                   2.85885980666130812e-04 * SOLAR_MASS),
 
-    'neptune': ([1.53796971148509165e+01,
-                 -2.59193146099879641e+01,
-                 1.79258772950371181e-01],
-                [2.68067772490389322e-03 * DAYS_PER_YEAR,
-                 1.62824170038242295e-03 * DAYS_PER_YEAR,
-                 -9.51592254519715870e-05 * DAYS_PER_YEAR],
-                5.15138902046611451e-05 * SOLAR_MASS) }
+        'uranus': ([1.28943695621391310e+01,
+                    -1.51111514016986312e+01,
+                    -2.23307578892655734e-01],
+                   [2.96460137564761618e-03 * DAYS_PER_YEAR,
+                    2.37847173959480950e-03 * DAYS_PER_YEAR,
+                    -2.96589568540237556e-05 * DAYS_PER_YEAR],
+                   4.36624404335156298e-05 * SOLAR_MASS),
 
-
-SYSTEM = BODIES.values()
-PAIRS = combinations(SYSTEM)
+        'neptune': ([1.53796971148509165e+01,
+                     -2.59193146099879641e+01,
+                     1.79258772950371181e-01],
+                    [2.68067772490389322e-03 * DAYS_PER_YEAR,
+                     1.62824170038242295e-03 * DAYS_PER_YEAR,
+                     -9.51592254519715870e-05 * DAYS_PER_YEAR],
+                    5.15138902046611451e-05 * SOLAR_MASS) }
 
 
-def advance(dt, n, bodies=SYSTEM, pairs=PAIRS):
+    system = bodies.values()
+    pairs = combinations(system)
+
+    return system, bodies, pairs
+
+
+def advance(dt, n, bodies, pairs):
 
     for i in xrange(n):
         for (([x1, y1, z1], v1, m1),
@@ -80,7 +91,7 @@ def advance(dt, n, bodies=SYSTEM, pairs=PAIRS):
             r[2] += dt * vz
 
 
-def report_energy(bodies=SYSTEM, pairs=PAIRS, e=0.0):
+def report_energy(bodies, pairs, e=0.0):
 
     for (((x1, y1, z1), v1, m1),
          ((x2, y2, z2), v2, m2)) in pairs:
@@ -90,10 +101,9 @@ def report_energy(bodies=SYSTEM, pairs=PAIRS, e=0.0):
         e -= (m1 * m2) / ((dx * dx + dy * dy + dz * dz) ** 0.5)
     for (r, [vx, vy, vz], m) in bodies:
         e += m * (vx * vx + vy * vy + vz * vz) / 2.
-    #print "%.9f" % e
-    "%.9f" % e
+    return e
 
-def offset_momentum(ref, bodies=SYSTEM, px=0.0, py=0.0, pz=0.0):
+def offset_momentum(ref, bodies, px=0.0, py=0.0, pz=0.0):
 
     for (r, [vx, vy, vz], m) in bodies:
         px -= vx * m
@@ -104,8 +114,19 @@ def offset_momentum(ref, bodies=SYSTEM, px=0.0, py=0.0, pz=0.0):
     v[1] = py / m
     v[2] = pz / m
 
-def run_iter(n, ref='sun'):
-    offset_momentum(BODIES[ref])
-    report_energy()
-    advance(0.01, n)
-    report_energy()
+def inner_iter(n, ref='sun'):
+    checksum = 0
+    system, bodies, pairs = setup_state()
+
+    offset_momentum(bodies[ref], system)
+    checksum += report_energy(system, pairs)
+    advance(0.01, n, system, pairs)
+    checksum += report_energy(system, pairs)
+
+    if abs(checksum - EXPECT_CHECKSUM) >= EPSILON:
+        print("bad checksum: %f vs %f" % (checksum, EXPECT_CHECKSUM))
+        sys.exit(1)
+
+def run_iter(n):
+    for i in xrange(n):
+        inner_iter(N_ADVANCES)
